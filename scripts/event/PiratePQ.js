c4233ee9d0f84ac26d0d2ce864bcf5d3e6d6fc78
SoupMS <tweetingchampion@gmail.com>
2025-08-06 05:54:03 -0500

    PQ and Showa changes

--- scripts/event/PiratePQ.js
+++ scripts/event/PiratePQ.js
@@ -21,13 +21,22 @@
 /**
  * @author: Ronan
  * @event: Pirate PQ
+
+ SoupMS Notes:
+
+ Pirate NPCs
+
+ Hat vendor
+ Wu Yang - 2094001 - Gives 1 feather as a reward.
+
+
  */
 
 var isPq = true;
 var isGrindMode = false;     // stages done after breaking all boxes on maps
 
-var minPlayers = 3, maxPlayers = 6;
-var minLevel = 55, maxLevel = 100;
+var minPlayers = 1, maxPlayers = 6;
+var minLevel = 55, maxLevel = 150;
 var entryMap = 925100000;
 var exitMap = 925100700;
 var recruitMap = 251010404;
